b9768fb07d093851ecfc9b20361efe118c56ff99
arfana017 <70667415+arfana017@users.noreply.github.com>
2020-10-26 23:46:43 -0700

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,35 @@
+class Dustbin {
+constructor(x,y,width,length) {
+
+    var options = {
+
+        isStatic:true,
+        friction:0.5,
+        density:1.2
+
+    }
+
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.length = length;
+    this.body = Bodies.rectangle(this.x,this.y,this.width,this.length)
+
+    World.add(world, this.body);
+
+}
+
+display() {
+
+    var pos = this.body.postion;
+    push();
+    rectMode(CENTER);
+    fill("lightblue");
+    rect(this.x,this.y,this.width,this.length);
+    pop();
+
+}
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor = "white";
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball = new Paper(200,650,20);
+	tBottom = new Dustbin(600,650,200,20);
+	tSide1 = new Dustbin(500,610,20,100);
+	tSide2 = new Dustbin(700,610,20,100);
+
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  ball.display();
+  tBottom.display();
+  tSide1.display();
+  tSide2.display();
+
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	   
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85});
+	
+	 }
+   }
+
+
+
