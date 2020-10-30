010db76cfd09f5de0bdf647f6cdc547bd6b40f64
arfana017 <70667415+arfana017@users.noreply.github.com>
2020-10-29 19:14:30 -0700

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -1,35 +1,22 @@
 class Dustbin {
-constructor(x,y,width,length) {
-
-    var options = {
-
-        isStatic:true,
-        friction:0.5,
-        density:1.2
-
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic: true,
+          'restitution':0,
+          'friction':1,
+          'density':0.1
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
     }
+    display(){
+      rectMode(CENTER);
 
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.length = length;
-    this.body = Bodies.rectangle(this.x,this.y,this.width,this.length,options)
+      fill(255);
 
-    World.add(world, this.body);
-
-}
-
-display() {
-
-    var pos = this.body.postion;
-    push();
-    rectMode(CENTER);
-    fill("lightblue");
-    rect(this.x,this.y,this.width,this.length);
-    pop();
-
-}
-
-
-
-}
+      rect(this.body.position.x, this.body.position.y, this.width, this. height);
+    }
+  };
